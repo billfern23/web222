b67768361b7d46831c42b6f484dae0660ad91b8b
billfern23 <56287260+billfern23@users.noreply.github.com>
2020-04-14 15:37:03 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 /** Add any JavaScript you need to this file. */
-var scale = 'scale(1)';
+
+window.onload = function showall() {
+ var scale = 'scale(1)';
 document.body.style.webkitTransform =  scale;    // Chrome, Opera, Safari
  document.body.style.msTransform =   scale;       // IE 9
  document.body.style.transform = scale; 
-window.onload = function showall() {
   document.querySelectorAll('.products').forEach(function(el) {
     el.style.display = 'block';
   });
